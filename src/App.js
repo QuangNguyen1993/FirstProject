4b0adf3c1464f14ed92a44b9d5036b7b094f3c72
NguyenQuang <vinhquang19930@gmail.com>
2020-12-16 18:44:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css';
+import RegisterForm from './components/register-form/RegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+        <RegisterForm></RegisterForm>
+    </div>
+  );
+}
+
+export default App;
